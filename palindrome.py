5ffb81070c0da8bc1075ee0ae1dac5cea2616ace
Kalashkhatod <kalashkhatod@gmail.com>
2026-02-27 13:28:13 +0530

    Made some changes

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+# class Solution(object):
+#     def isPalindrome(self, num):
+#         digit = ""
+#         if num >= 0:
+#             for char in str(num)[::-1]:
+#                 digit += char
+#             digit = int(digit)
+#             if num == digit:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return False
+
+#  using another logic
+
+def solve(num):
+    num = str(num)
+    print(num[::-1])
+    return num[::-1] == num
+
+print(solve(+121))
